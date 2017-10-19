e96be46737cf8029c324d15148f530d8d35fec4e
Moonmoore <32908908+Moonmoore@users.noreply.github.com>
2017-10-18 21:31:39 -0400

    Update Assignment_4.cpp

--- Assignment_4.cpp
+++ Assignment_4.cpp
@@ -1,1 +1,4 @@
-/* This project has been created by Moon Moore, Brayden Lloyd, 
+/* This project has been created by Moon Moore, Brayden Lloyd, Matt Kernan, and Hailey McLaughin
+Date: 10/18/17
+The purpose of this assignment is to create POS program that enables a trained employee to take pizza orders,
+as well as keep track of profit and the amount of all denominations of money in the register.
